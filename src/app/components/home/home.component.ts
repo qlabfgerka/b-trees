0852a7dd52e55fa2a87bd78fc629cb97d2271240
Jan Premzl <jan.premzl16@gmail.com>
2022-01-18 20:41:19 +0100

    Improvements

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -14,51 +14,63 @@ export class HomeComponent implements OnInit {
   public degree!: number;
   public text!: string;
   public nodeID!: number;
+  public array!: Array<number>;
+  public interval!: any;
+  public degreeSet: boolean = false;
 
   constructor(private readonly treeService: TreeService) {}
 
   ngOnInit(): void {
-    this.degree = 2;
     this.tree = this.treeService.initializeTree(this.tree);
     this.text = '';
+    this.array = new Array<number>();
+  }
+
+  public async changeDegree(degree: number): Promise<void> {
+    this.degreeSet = true;
+    this.degree = degree;
+    this.text = '';
+    this.tree = this.treeService.initializeTree(this.tree);
+
+    const temp = this.array;
+    this.array = new Array<number>();
+
+    if (temp.length > 0) {
+      for (let i = 0; i < temp.length; i++) {
+        this.insert(+temp[i]);
+        await this.sleep();
+      }
+    }
   }
 
   public insert(key: number): void {
     if (!this.tree) return;
 
+    this.array.push(key);
+
     this.tree = this.treeService.insert(this.tree, key, this.degree);
 
-    //console.log(this.tree);
-    //this.displayTree(this.tree);
     this.nodeID = 0;
     this.text = '';
     this.displayTree(this.tree, 0, 0);
-    //console.log(this.text);
     this.drawNodes(this.tree, 0);
   }
 
-  public openFile(event: any): void {
+  public async openFile(event: any): Promise<void> {
     let array: Array<number> = new Array<number>();
     const reader = new FileReader();
-    reader.onload = (e: any) => {
+    reader.onload = async (e: any) => {
       array = e.target.result.split(' ');
-      let i = 0;
-      setInterval(() => {
-        if (i < array.length) {
-          this.insert(+array[i]);
-          ++i;
-          if (i == array.length) {
-          }
-        }
-      }, 2000);
+      for (let i = 0; i < array.length; i++) {
+        this.insert(+array[i]);
+        await this.sleep();
+      }
     };
     reader.readAsText(event.files[0]);
   }
 
   private displayTree(tree: Node, fIndex: number, sender: number): void {
     let j;
-    console.log(tree);
-    console.log('ID: ' + this.nodeID + ' <f' + fIndex + '> FROM: ' + sender);
 
     this.text += `node${this.nodeID}[label ="`;
     for (j = 0; j < tree.currentKeys!; j++) {
@@ -67,9 +79,7 @@ export class HomeComponent implements OnInit {
     if (sender == 0 && this.nodeID == 0) {
       this.text += `<f${j}>"];`;
     } else {
-      this.text += `<f${j}>"];\n"node${sender}":f${fIndex} -> "node${
-        this.nodeID
-      }"\n
+      this.text += `<f${j}>"];\n"node${sender}":f${fIndex} -> "node${this.nodeID}"\n
       `;
     }
     ++this.nodeID;
@@ -79,7 +89,6 @@ export class HomeComponent implements OnInit {
 
     while (i < tree.currentKeys!) {
       if (!tree.leaf) this.displayTree(tree.children![i], i, id - 1);
-      //console.log(tree.keys![i]);
       ++i;
     }
     if (!tree.leaf) this.displayTree(tree.children![i], i, id - 1);
@@ -92,5 +101,9 @@ export class HomeComponent implements OnInit {
       ${this.text}
      }
     `);
+  }
+
+  private sleep() {
+    return new Promise((resolve) => setTimeout(resolve, 1000));
   }
 }
